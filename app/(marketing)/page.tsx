b302060d1ef206b3c1e2040325197eb4d851efac
snapees <93643707+snapees@users.noreply.github.com>
2025-02-05 15:51:19 +0530

    `Added language selection buttons to footer and centered content on marketing page`

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           Learn, Practice and Master New Languages with Lingo
         </h1>
 
-        <div>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
           <ClerkLoading>
             <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
           </ClerkLoading>
