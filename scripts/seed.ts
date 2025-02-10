5e48d2424e4ce1c5039ebe03ec93c0f00605b2d8
snapees <iamamitthakur2602@gmail.com>
2025-02-10 18:21:16 +0530

    `feat: add units and lessons data to db and update learn page to display units`

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -12,6 +12,11 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values([
       {
@@ -38,6 +43,125 @@ const main = async () => {
         id: 5,
         title: "Japanease",
         imageSrc: "/jp.svg",
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1, // Spanish
+        title: "Unit 1",
+        description: "Learn the basics of Spanish",
+        order: 1,
+      },
+      // {
+      //   id: 2,
+      //   courseId: 2, // Italian
+      //   title: "Unit 1",
+      //   description: "Learn the basics of Italian",
+      //   order: 2,
+      // },
+      // {
+      //   id: 3,
+      //   courseId: 3, // French
+      //   title: "Unit 1",
+      //   description: "Learn the basics of French",
+      //   order: 3,
+      // },
+      // {
+      //   id: 4,
+      //   courseId: 4, // Croatian
+      //   title: "Unit 1",
+      //   description: "Learn the basics of Croatian",
+      //   order: 4,
+      // },
+      // {
+      //   id: 5,
+      //   courseId: 5, // Japanease
+      //   title: "Unit 1",
+      //   description: "Learn the basics of Japanease",
+      //   order: 5,
+      // },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1, // Unit 1 (Learn the basics...)
+        order: 1,
+        title: "Nouns",
+      },
+      // {
+      //   id: 2,
+      //   unitId: 1, // Unit 1 (Learn the basics...)
+      //   order: 2,
+      //   title: "Verbs",
+      // },
+      // {
+      //   id: 3,
+      //   unitId: 1, // Unit 1 (Learn the basics...)
+      //   order: 3,
+      //   title: "Verbs",
+      // },
+      // {
+      //   id: 4,
+      //   unitId: 1, // Unit 1 (Learn the basics...)
+      //   order: 4,
+      //   title: "Verbs",
+      // },
+      // {
+      //   id: 5,
+      //   unitId: 1, // Unit 1 (Learn the basics...)
+      //   order: 5,
+      //   title: "Verbs",
+      // },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1, // Nouns
+        type: "SELECT",
+        order: 1,
+        question: 'Which one of these is the "the man"?',
+      },
+      // {
+      //   id: 2,
+      //   lessonId: 2, // Nouns
+      //   type: "ASSIST",
+      //   order: 2,
+      //   question: '"the man"',
+      // },
+      // {
+      //   id: 3,
+      //   lessonId: 1, // Nouns
+      //   type: "SELECT",
+      //   order: 3,
+      //   question: 'Which one of these is the "the robot"?',
+      // },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        challengeId: 1, // Which one of these is "the man"?
+        imageSrc: "/man.svg",
+        correct: true,
+        text: "el hombre",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        challengeId: 1,
+        imageSrc: "/woman.svg",
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        challengeId: 1,
+        imageSrc: "/robot.svg",
+        correct: false,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
       },
     ]);
 
