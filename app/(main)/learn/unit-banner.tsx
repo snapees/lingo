33701c031e942bdcf37325b4524023b89f059b64
snapees <iamamitthakur2602@gmail.com>
2025-02-12 13:28:50 +0530

    `feat: updated unit-banner and quiz components with minor styling and layout changes`

--- app/(main)/learn/unit-banner.tsx
+++ app/(main)/learn/unit-banner.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export const UnitBanner = ({ title, description }: Props) => {
   return (
-    <div className="w-full rounded-xl bg-green-500 p-5 text-white flex items-center justify-between">
+    <div className="w-full rounded-xl bg-green-500 p-5 text-white flex items-center justify-between mb-5">
       <div className="space-y-2.5">
         <h3 className="text-2xl font-bold">{title}</h3>
         <p className="text-lg">{description}</p>
