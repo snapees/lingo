4e3d2bca91e576d90dfd050e1ff3744090cc78fd
snapees <iamamitthakur2602@gmail.com>
2025-02-14 13:56:21 +0530

    Shop page working

---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,63 @@
+import db from "@/db/drizzle";
+import { userSubscriptions } from "@/db/schema";
+import { stripe } from "@/lib/stripe";
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  //   @ts-ignore
+  const signature = headers().get("Stripe-Signature") as string;
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error: any) {
+    return new NextResponse(`Webhook error: ${error.message}`, { status: 400 });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+
+  if (event.type === "checkout.session.completed") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+
+    if (!session?.metadata?.userId) {
+      return new NextResponse("User Id is Required", { status: 200 });
+    }
+
+    await db.insert(userSubscriptions).values({
+      userId: session.metadata.userId,
+      stripeSubscriptionId: subscription.id,
+      stripeCustomerId: subscription.customer as string,
+      stripePriceId: subscription.items.data[0].price.id,
+      stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
+    });
+  }
+
+  if (event.type === "invoice.payment_succeeded") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+
+    await db
+      .update(userSubscriptions)
+      .set({
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      })
+      .where(eq(userSubscriptions.stripeSubscriptionId, subscription.id));
+  }
+
+  return new NextResponse(null, { status: 200 });
+}
--- db/queries.ts
+++ db/queries.ts
@@ -9,6 +9,7 @@ import {
   lessons,
   units,
   userProgress,
+  userSubscriptions,
 } from "@/db/schema";
 
 export const getUserProgress = cache(async () => {
@@ -208,4 +209,27 @@ export const getLessonPercentage = cache(async () => {
   );
 
   return percentage;
+});
+
+const DAY_IN_MS = 86_400_000;
+
+export const getUserSubscription = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) return null;
+
+  const data = await db.query.userSubscriptions.findFirst({
+    where: eq(userSubscriptions.userId, userId),
+  });
+
+  if (!data) return null;
+
+  const isActive =
+    data.stripePriceId &&
+    data.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+
+  return {
+    ...data,
+    isActive,
+  };
 });
