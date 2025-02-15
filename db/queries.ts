461f06dd70ee2378d14f9bdb52bc992fe6120fcd
snapees <iamamitthakur2602@gmail.com>
2025-02-15 16:29:15 +0530

    `Added @radix-ui/react-avatar and @radix-ui/react-separator dependencies`

--- db/queries.ts
+++ db/queries.ts
@@ -233,3 +233,22 @@ export const getUserSubscription = cache(async () => {
     isActive,
   };
 });
+
+export const getTopTenUsers = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) return [];
+
+  const data = await db.query.userProgress.findMany({
+    orderBy: (userProgress, { desc }) => [desc(userProgress.points)],
+    limit: 10,
+    columns: {
+      userId: true,
+      userName: true,
+      userImageSrc: true,
+      points: true,
+    },
+  });
+
+  return data;
+});
